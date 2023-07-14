40c5e38a9028563c59d5710030d38992021809d8
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-07-14 16:49:43 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import Counter from './Counter/Counter'
+import Header from './Header/Header';
+import Modal from './Modal/Modal';
+import FormSignUp from './Forms/FormSignUp/FormSignUp';
+import { nanoid } from 'nanoid';
+// import FormikSignUp from './Forms/FormSignUp/FormikSignUp'
+// import UsersList from './Users/UsersList/UsersList'
+import { Toaster } from 'react-hot-toast';
+import Users from './Users/Users';
+
+class App extends Component {
+  state = {
+    isShowModal: false,
+  };
+
+  openModal = () => {
+    this.setState({ isShowModal: true });
+  };
+
+  closeModal = () => {
+    this.setState({ isShowModal: false });
+  };
+
+  createUser = data => {
+    const newUser = {
+      id: nanoid(),
+      isDev: true,
+      ...data,
+    };
+    console.log(newUser);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Toaster />
+        <Header
+          handleShowModal={this.openModal}
+          handleShowModal2={this.openModal2}
+        />
+        {/* <Counter /> */}
+        {this.state.isShowModal && (
+          <Modal closeModal={this.closeModal}>
+            {/* <FormikSignUp
+							closeModal={this.closeModal}
+							createUser={this.createUser}
+						/> */}
+            <FormSignUp
+              closeModal={this.closeModal}
+              createUser={this.createUser}
+            />
+          </Modal>
+        )}
+        <Users />
+      </div>
+    );
+  }
+}
+export default App;
